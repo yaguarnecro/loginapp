e651719778ad719ef38b4db2c0906e72c9c8fa8c
yaguarnecro <yaguarnecro@gmail.com>
2020-12-01 08:39:07 -0500

    creación instancia usuarioModel

---
+++ src/app/models/usuario.model.ts
@@ -0,0 +1,8 @@
+// es una clase de tyscript, para los campos del formulario,
+// con instancias de este modelo, asociamos los datos al objeto.
+
+export class usuarioModel{
+    email: string;
+    password: string;
+    nombre: string;
+}
